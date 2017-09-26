8014e4191a0b35a6373ff9ccadfbb665f4c90c75
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-09-26 14:18:37 -0400

    MATTHEW: added music to start and hubworld and kept music playing between scenes.

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -5,6 +5,16 @@ public class BackgroundMusic : MonoBehaviour
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1 && objs[0].GetComponent<BackgroundMusic>().audioSource.clip.name == objs[1].GetComponent<BackgroundMusic>().audioSource.clip.name)
+        {
+            Destroy(this.gameObject);
+        }
+        else if(objs.Length > 1 && objs[0].GetComponent<BackgroundMusic>().audioSource.clip.name != objs[1].GetComponent<BackgroundMusic>().audioSource.clip.name)
+        {
+            Destroy(objs[0]);
+        }
+        DontDestroyOnLoad(audioSource);
     }
     private void Start()
     {
