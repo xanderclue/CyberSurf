22be377b690920d4ad237b9bfe10106215cdda11
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-09-27 14:25:00 -0400

    The major savename bug in the builds is finally fixed.. blame TextMeshPro

--- Assets/Scripts/Buttons/downLetterScript.cs
+++ Assets/Scripts/Buttons/downLetterScript.cs
@@ -3,25 +3,14 @@ using TMPro;
 public class downLetterScript : SelectedObject
 {
     [SerializeField]
-    TextMeshPro myLetter;
+    TextMeshPro textMesh;
     public override void selectSuccessFunction()
     {
-        BuildDebugger.WriteLine("TextMesh is " + myLetter.text);
-        char letter = myLetter.text[0];
-        BuildDebugger.WriteLine("Letter is " + letter);
-        if (letter <= 'A')
-        {
-            BuildDebugger.WriteLine(letter + " IS <=A");
+        textMesh.ForceMeshUpdate();
+        char letter = textMesh.GetParsedText()[0];
+        --letter;
+        if (letter < 'A')
             letter = 'Z';
-            BuildDebugger.WriteLine("Set Letter to " + letter);
-        }
-        else
-        {
-            BuildDebugger.WriteLine(letter + " ISNOT <=A");
-            --letter;
-            BuildDebugger.WriteLine("Set Letter to " + letter);
-        }
-        myLetter.SetText(letter.ToString());
-        BuildDebugger.WriteLine("TextMesh Set to " + myLetter.text);
+        textMesh.SetText(letter.ToString());
     }
 }
