15bfd6f60fa6307a3081e2f7603ebabef11a0670
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-09 10:24:47 -0400

    and now this

--- Assets/Scripts/Hub World/AudioMenuTab.cs
+++ Assets/Scripts/Hub World/AudioMenuTab.cs
@@ -2,28 +2,40 @@
 using TMPro;
 public class AudioMenuTab : MenuTab
 {
-    [SerializeField]
-    private TextMeshPro bgmVolume = null, sfxVolume = null, envVolume = null;
+    [SerializeField, Range(2, 1000), Tooltip("Number representing the maximum volume level")] private int maxNumber = 16;
+    [Space, SerializeField, LabelOverride("BGM Volume Text")] private TextMeshPro bgmVolume = null;
+    [SerializeField, LabelOverride("SFX Volume Text")] private TextMeshPro sfxVolume = null;
+    [SerializeField, LabelOverride("ENV Volume Text")] private TextMeshPro envVolume = null;
+    [Space, SerializeField] private EventSelectedObject bgmUpButton = null;
+    [SerializeField] private EventSelectedObject bgmDownButton = null;
+    [SerializeField] private EventSelectedObject sfxUpButton = null;
+    [SerializeField] private EventSelectedObject sfxDownButton = null;
+    [SerializeField] private EventSelectedObject envUpButton = null;
+    [SerializeField] private EventSelectedObject envDownButton = null;
+    private float volumeIncrement { get { return 1.0f / maxNumber; } }
     private void SetBgmText()
     {
-        if (null != bgmVolume)
-            bgmVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.BgmVolume * 16.0f).ToString());
+        bgmVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.BgmVolume * maxNumber).ToString());
     }
     private void SetSfxText()
     {
-        if (null != sfxVolume)
-            sfxVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.SfxVolume * 16.0f).ToString());
+        sfxVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.SfxVolume * maxNumber).ToString());
     }
     private void SetEnvText()
     {
-        if (null != envVolume)
-            envVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.EnvVolume * 16.0f).ToString());
+        envVolume.SetText(Mathf.RoundToInt(AudioLevels.Instance.EnvVolume * maxNumber).ToString());
     }
     private void OnEnable()
     {
         AudioLevels.Instance.OnBgmVolumeChange += SetBgmText;
         AudioLevels.Instance.OnSfxVolumeChange += SetSfxText;
         AudioLevels.Instance.OnEnvVolumeChange += SetEnvText;
+        bgmUpButton.OnSelectSuccess += BgmUp;
+        bgmDownButton.OnSelectSuccess += BgmDown;
+        sfxUpButton.OnSelectSuccess += SfxUp;
+        sfxDownButton.OnSelectSuccess += SfxDown;
+        envUpButton.OnSelectSuccess += EnvUp;
+        envDownButton.OnSelectSuccess += EnvDown;
         SetBgmText();
         SetSfxText();
         SetEnvText();
@@ -37,5 +49,35 @@ public class AudioMenuTab : MenuTab
             AudioLevels.Instance.OnEnvVolumeChange -= SetEnvText;
         }
         catch { }
+        bgmUpButton.OnSelectSuccess -= BgmUp;
+        bgmDownButton.OnSelectSuccess -= BgmDown;
+        sfxUpButton.OnSelectSuccess -= SfxUp;
+        sfxDownButton.OnSelectSuccess -= SfxDown;
+        envUpButton.OnSelectSuccess -= EnvUp;
+        envDownButton.OnSelectSuccess -= EnvDown;
+    }
+    private void BgmUp()
+    {
+        AudioLevels.Instance.BgmVolume += volumeIncrement;
+    }
+    private void BgmDown()
+    {
+        AudioLevels.Instance.BgmVolume -= volumeIncrement;
+    }
+    private void SfxUp()
+    {
+        AudioLevels.Instance.SfxVolume += volumeIncrement;
+    }
+    private void SfxDown()
+    {
+        AudioLevels.Instance.SfxVolume -= volumeIncrement;
+    }
+    private void EnvUp()
+    {
+        AudioLevels.Instance.EnvVolume += volumeIncrement;
+    }
+    private void EnvDown()
+    {
+        AudioLevels.Instance.EnvVolume -= volumeIncrement;
     }
 }
