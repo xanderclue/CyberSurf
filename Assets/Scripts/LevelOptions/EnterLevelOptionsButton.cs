8b6c2179329a363bb9bfb61aed2c81432b8fb5ac
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-10 14:00:42 -0400

    changed material on menu and level floating things

--- Assets/Scripts/LevelOptions/EnterLevelOptionsButton.cs
+++ Assets/Scripts/LevelOptions/EnterLevelOptionsButton.cs
@@ -6,16 +6,37 @@ public class EnterLevelOptionsButton : SelectedObject
     private LevelMenuStuff levelMenuScript = null;
     [SerializeField, Tooltip("degrees per second")]
     private float rotationSpeed = 111.0f;
+    [SerializeField]
+    private Material hoverMat = null;
+    [SerializeField]
+    private Material noHoverMat = null;
+    private MeshRenderer meshRenderer = null;
     new private void Start()
     {
         base.Start();
+        meshRenderer = GetComponent<MeshRenderer>();
         if (null == levelMenuScript)
             levelMenuScript = GetComponentInParent<LevelMenuStuff>();
+        if (null == noHoverMat)
+            noHoverMat = meshRenderer.material;
+        if (null == hoverMat)
+            hoverMat = noHoverMat;
+        meshRenderer.material = noHoverMat;
     }
     new private void Update()
     {
         base.Update();
         gameObject.transform.Rotate(0.0f, Time.deltaTime * rotationSpeed, 0.0f);
+    }
+    protected override void SelectedFunction()
+    {
+        base.SelectedFunction();
+        meshRenderer.material = hoverMat;
+    }
+    protected override void DeselectedFunction()
+    {
+        base.DeselectedFunction();
+        meshRenderer.material = noHoverMat;
     }
     public override void SuccessFunction()
     {
