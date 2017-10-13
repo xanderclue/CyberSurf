a6802f1fc00f02dfc9ab563c827627751faf979b
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-13 12:26:32 -0400

    NATHAN: Minor adjustments.

--- Assets/Scripts/Camera Scripts/GamepadCameraController.cs
+++ Assets/Scripts/Camera Scripts/GamepadCameraController.cs
@@ -7,13 +7,12 @@ using UnityEngine.SceneManagement;
 public class GamepadCameraController : MonoBehaviour
 {
     [SerializeField, Range(10f, 100f)] float firstPersonCameraSpeed = 60f;
-    float thirdPersonCameraSpeed = 90f;
+    [SerializeField, Range(30f, 120f)] float thirdPersonCameraSpeed = 90f;
 
-    Transform cameraContainerTransform;
     Transform playerTransform;
-    ThirdPersonCamera thirdPersonCamera;
     Rigidbody playerRB;
-
+    Transform cameraContainerTransform;
+    ThirdPersonCamera thirdPersonCamera;
     Vector3 thirdPersonTranslation;
 
     void Start ()
@@ -31,20 +30,17 @@ public class GamepadCameraController : MonoBehaviour
 
     private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
     {
-        //reset our camera position to the player's rotation, if we were using debug camera rotation
+        //reset our camera position to the player's rotation whenever we change scenes
         if (!VRDevice.isPresent)
             cameraContainerTransform.eulerAngles = playerRB.transform.eulerAngles;
     }
 
     void ThirdPersonCameraMove()
     {
-        cameraContainerTransform.position = thirdPersonCamera.FirstPersonAnchor.position;
-
         float cameraPitch = cameraContainerTransform.eulerAngles.x + -Input.GetAxis("RVertical") * thirdPersonCameraSpeed * Time.deltaTime;
         float cameraYaw = cameraContainerTransform.eulerAngles.y + Input.GetAxis("RHorizontal") * thirdPersonCameraSpeed * Time.deltaTime;
 
-        //make the pitch not go above 90, and not go below 335
-
+        //make the pitch not go above 80 degrees, and not go below 335 degrees
         cameraPitch = Quaternion.Euler(new Vector3(cameraPitch, 0f, 0f)).eulerAngles.x;
 
         //camera position is lower than the player
@@ -59,10 +55,9 @@ public class GamepadCameraController : MonoBehaviour
                 cameraPitch = 80f;
         }
 
-        print("Camera Pitch: " + cameraPitch);
-
         cameraContainerTransform.rotation = (Quaternion.Euler(new Vector3(cameraPitch, cameraYaw, 0f)));
 
+        cameraContainerTransform.position = thirdPersonCamera.FirstPersonAnchor.position;
         cameraContainerTransform.Translate(thirdPersonTranslation);
     }
 
@@ -87,10 +82,8 @@ public class GamepadCameraController : MonoBehaviour
         //while updating to the third person camera, re-align the camera 
         if (thirdPersonCamera.UpdatingCameraPosition)
             ReAlignCamera();
-
         else if (thirdPersonCamera.UsingThirdPersonCamera)
             ThirdPersonCameraMove();
-
         else
             FirstPersonCameraMove();
 
