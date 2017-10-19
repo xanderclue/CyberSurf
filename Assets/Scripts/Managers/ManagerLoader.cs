f26f6b4a9c82db566f48cb1d4b9f90257468f92e
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-19 11:43:03 -0400

    setting steam appid

--- Assets/Scripts/Managers/ManagerLoader.cs
+++ Assets/Scripts/Managers/ManagerLoader.cs
@@ -8,6 +8,8 @@ public class ManagerLoader : MonoBehaviour
     [SerializeField]
     private List<GameObject> destroyOnLoad = null;
 
+    private static bool isLoaded = false;
+
     //loads our GameManager into the scene,
     //      By doing this instead of having our GameManger already in the scene, we
     //      prevent attached scripts from calling Awake() more than once.
@@ -16,8 +18,23 @@ public class ManagerLoader : MonoBehaviour
         if (null != destroyOnLoad)
             foreach (GameObject gObj in destroyOnLoad)
                 try { gObj.SetActive(false); Destroy(gObj); } catch { }
-        if (null == GameManager.instance)
-            Instantiate(gameManager);
+        if (!isLoaded)
+        {
+            BuildDebugger.InitDebugger();
+            if (null == GameManager.instance)
+                Instantiate(gameManager);
+#if UNITY_EDITOR
+            try
+            {
+                const int appid = 735810;
+                System.IO.StreamWriter steamAppIdEditor = new System.IO.StreamWriter(Application.dataPath + "/../steam_appid.txt", false, System.Text.Encoding.ASCII);
+                steamAppIdEditor.Write(appid);
+                steamAppIdEditor.Close();
+            }
+            catch { }
+#endif
+            isLoaded = true;
+        }
         try { Destroy(gameObject); } catch { }
     }
 }
