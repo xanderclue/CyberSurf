619bce30d8ef842720c6313b22a099b784349c48
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-20 13:10:59 -0400

    it's okay

--- Assets/Jons stuff/Jons Scripts/turnOffandLerpOnObjects.cs
+++ Assets/Jons stuff/Jons Scripts/turnOffandLerpOnObjects.cs
@@ -1,81 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class turnOffandLerpOnObjects : MonoBehaviour
 {
-    Color portalInvis;
-    Color portalVis;
+    [SerializeField, LabelOverride("Objects")] private GameObject[] objs = null;
 
-    Color menuInvis;
-    Color menuVis;
-
-    Color[] standsInvis;
-    Color[] standsVis;
-
-    Renderer portal;
-    Renderer menu;
-    Renderer[] stands;
-
-    GameObject stds;
-    GameObject portals;
-    GameObject menus;
-
-    float currentTime;
-    //float timeToPlay;
-    // Use this for initialization
     void Start()
     {
-        //timeToPlay = 15.0f;
-
-        stds = GameObject.FindGameObjectWithTag("boardStands");
-        portals = GameObject.FindGameObjectWithTag("worldPortals");
-        menus = GameObject.FindGameObjectWithTag("menuSystem");
-
-        stds.SetActive(false);
-        portals.SetActive(false);
-        menus.SetActive(false);
-        //  portal = GameObject.Find("DemoPortal").GetComponentInChildren<Renderer>();
-        //  menu = GameObject.Find("EnterMainMenuButton").GetComponent<Renderer>();
-        //  stands = GameObject.Find("BoardStands").GetComponentsInChildren<Renderer>(true);
-        //
-        //
-        //  portalInvis = portalVis;
-        //  portalInvis.a = 0;
-        //
-        //  menuInvis = menuVis;
-        //  menuInvis.a = 0;
-        //
-        //  for(int i = 0; i < stands.Length; i++)
-        //  {
-        //      standsInvis[i] = standsVis[i];
-        //      standsInvis[i].a = 0;
-        //      stands[i].material.color = standsInvis[i];
-        //  }
-        //
-        //
-        //  portal.gameObject.SetActive(false);
-        //
-        // 
-        //  menu.material.color = menuInvis;
-
+        if (keepPlayerStill.tutorialOn)
+            foreach (GameObject obj in objs)
+                obj.SetActive(false);
+        else Destroy(this);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (keepPlayerStill.tutorialOn == false)
+        if (!keepPlayerStill.tutorialOn)
         {
-               stds.SetActive(true);
-            portals.SetActive(true);
-              menus.SetActive(true);
-            //  currentTime += Time.deltaTime;
-            //  portal.material.color = Color.Lerp(portalInvis, portalVis, currentTime / timeToPlay);
-            //  menu.material.color = Color.Lerp(menuInvis, menuVis, currentTime / timeToPlay);
-            //  for(int i = 0; i < stands.Length; i++)
-            //  {
-            //      stands[i].material.color = Color.Lerp(standsInvis[i], standsVis[i], currentTime / timeToPlay);
-            //  }
+            foreach (GameObject obj in objs)
+                obj.SetActive(true);
+            Destroy(this);
         }
     }
 }
