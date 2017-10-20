cbbd4a081a3781af1148b5e18ae1ae133bd8a670
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-20 12:54:59 -0400

    NATHAN: Rocks in canyon level. (+2 more updates)

---
+++ Assets/Scripts/CanyonRockRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanyonRockRotator : MonoBehaviour
+{
+    Rigidbody rockRigidBody;
+
+    [SerializeField] float angularVelocityLimit = 10f;
+    [SerializeField] float xTorqueAmount = 10f;
+    [SerializeField] float yTorqueAmount = 10f;
+    [SerializeField] float zTorqueAmount = 10f;
+
+    Vector3 torqueVector;
+
+	void Start ()
+    {
+        rockRigidBody = GetComponent<Rigidbody>();
+        torqueVector = new Vector3(xTorqueAmount, yTorqueAmount, zTorqueAmount);
+	}
+
+    private void FixedUpdate()
+    {
+        if (rockRigidBody.angularVelocity.magnitude < angularVelocityLimit)
+            rockRigidBody.AddTorque(torqueVector);
+    }
+}
