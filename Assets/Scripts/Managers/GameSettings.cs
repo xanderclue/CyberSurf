7367ba9c6c5309016f205821f14f70ae34beeeab
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-25 14:26:53 -0400

    more game-breaking stuff

--- Assets/Scripts/Managers/GameSettings.cs
+++ Assets/Scripts/Managers/GameSettings.cs
@@ -3,104 +3,134 @@ public static class GameSettings
 {
     public static bool SetBool(string key, bool value)
     {
-        PlayerPrefs.SetInt(key + "BOOL", value ? 1 : 0);
+        PlayerPrefs.SetInt(key + "_BOOL", value ? 1 : 0);
         return value;
     }
     public static bool GetBool(string key, bool defaultValue = false)
     {
-        return SetBool(key, 0 != PlayerPrefs.GetInt(key + "BOOL", defaultValue ? 1 : 0));
+        return SetBool(key, 0 != PlayerPrefs.GetInt(key + "_BOOL", defaultValue ? 1 : 0));
     }
     public static bool GetBool(string key, ref bool value)
     {
-        value = GetBool(key, value);
-        return value;
+        return value = GetBool(key, value);
     }
+    public static void ResetBool(string key)
+    {
+        key += "_BOOL";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static int SetInt(string key, int value)
     {
-        PlayerPrefs.SetInt(key + "INT", value);
+        PlayerPrefs.SetInt(key + "_INT", value);
         return value;
     }
     public static int GetInt(string key, int defaultValue = 0)
     {
-        return SetInt(key, PlayerPrefs.GetInt(key + "INT", defaultValue));
+        return SetInt(key, PlayerPrefs.GetInt(key + "_INT", defaultValue));
     }
     public static int GetInt(string key, ref int value)
     {
-        value = GetInt(key, value);
-        return value;
+        return value = GetInt(key, value);
     }
+    public static void ResetInt(string key)
+    {
+        key += "_INT";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static uint SetUint(string key, uint value)
     {
-        PlayerPrefs.SetInt(key + "UINT", unchecked((int)value));
+        PlayerPrefs.SetInt(key + "_UINT", (int)value);
         return value;
     }
     public static uint GetUint(string key, uint defaultValue = 0)
     {
-        return SetUint(key, unchecked((uint)PlayerPrefs.GetInt(key + "UINT", unchecked((int)defaultValue))));
+        return SetUint(key, (uint)PlayerPrefs.GetInt(key + "_UINT", (int)defaultValue));
     }
     public static uint GetUint(string key, ref uint value)
     {
-        value = GetUint(key, value);
-        return value;
+        return value = GetUint(key, value);
     }
+    public static void ResetUint(string key)
+    {
+        key += "_UINT";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static float SetFloat(string key, float value)
     {
-        PlayerPrefs.SetFloat(key + "FLOAT", value);
+        PlayerPrefs.SetFloat(key + "_FLOAT", value);
         return value;
     }
     public static float GetFloat(string key, float defaultValue = 0.0f)
     {
-        return SetFloat(key, PlayerPrefs.GetFloat(key + "FLOAT", defaultValue));
+        return SetFloat(key, PlayerPrefs.GetFloat(key + "_FLOAT", defaultValue));
     }
     public static float GetFloat(string key, ref float value)
     {
-        value = GetFloat(key, value);
-        return value;
+        return value = GetFloat(key, value);
     }
+    public static void ResetFloat(string key)
+    {
+        key += "_FLOAT";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static string SetString(string key, string value)
     {
-        PlayerPrefs.SetString(key + "STRING", value);
+        PlayerPrefs.SetString(key + "_STRING", value);
         return value;
     }
     public static string GetString(string key, string defaultValue = "")
     {
-        return SetString(key, PlayerPrefs.GetString(key + "STRING", defaultValue));
+        return SetString(key, PlayerPrefs.GetString(key + "_STRING", defaultValue));
     }
     public static string GetString(string key, ref string value)
     {
-        value = GetString(key, value);
-        return value;
+        return value = GetString(key, value);
     }
+    public static void ResetString(string key)
+    {
+        key += "_STRING";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static char SetChar(string key, char value)
     {
-        PlayerPrefs.SetString(key + "CHAR", "" + value);
+        PlayerPrefs.SetInt(key + "_CHAR", value);
         return value;
     }
     public static char GetChar(string key, char defaultValue = '\0')
     {
-        string str = PlayerPrefs.GetString(key + "CHAR", "" + defaultValue);
-        return SetChar(key, (null != str && str.Length > 0) ? str[0] : '\0');
+        return SetChar(key, (char)PlayerPrefs.GetInt(key + "_CHAR", defaultValue));
     }
     public static char GetChar(string key, ref char value)
     {
-        value = GetChar(key, value);
-        return value;
+        return value = GetChar(key, value);
     }
+    public static void ResetChar(string key)
+    {
+        key += "_CHAR";
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
     public static Color SetColor(string key, Color value)
     {
-        PlayerPrefs.SetFloat(key + "COLORR", value.r);
-        PlayerPrefs.SetFloat(key + "COLORG", value.g);
-        PlayerPrefs.SetFloat(key + "COLORB", value.b);
-        PlayerPrefs.SetFloat(key + "COLORA", value.a);
+        key += "_COLOR_";
+        PlayerPrefs.SetFloat(key + 'R', value.r);
+        PlayerPrefs.SetFloat(key + 'G', value.g);
+        PlayerPrefs.SetFloat(key + 'B', value.b);
+        PlayerPrefs.SetFloat(key + 'A', value.a);
         return value;
     }
     public static Color GetColor(string key, Color defaultValue)
     {
         Color ret;
-        ret.r = PlayerPrefs.GetFloat(key + "COLORR", defaultValue.r);
-        ret.g = PlayerPrefs.GetFloat(key + "COLORG", defaultValue.g);
-        ret.b = PlayerPrefs.GetFloat(key + "COLORB", defaultValue.b);
-        ret.a = PlayerPrefs.GetFloat(key + "COLORA", defaultValue.a);
+        ret.r = PlayerPrefs.GetFloat(key + "_COLOR_R", defaultValue.r);
+        ret.g = PlayerPrefs.GetFloat(key + "_COLOR_G", defaultValue.g);
+        ret.b = PlayerPrefs.GetFloat(key + "_COLOR_B", defaultValue.b);
+        ret.a = PlayerPrefs.GetFloat(key + "_COLOR_A", defaultValue.a);
         return SetColor(key, ret);
     }
     public static Color GetColor(string key)
@@ -109,36 +139,146 @@ public static class GameSettings
     }
     public static Color GetColor(string key, ref Color value)
     {
-        value = GetColor(key, value);
-        return value;
+        return value = GetColor(key, value);
     }
-    public static T GetEnum<T>(string key, ref T value) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
+    public static void ResetColor(string key)
     {
-        if (!typeof(T).IsEnum)
-            throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
-        value = GetEnum(key, value);
+        key += "_COLOR_";
+        if (PlayerPrefs.HasKey(key + 'R')) PlayerPrefs.DeleteKey(key + 'R');
+        if (PlayerPrefs.HasKey(key + 'G')) PlayerPrefs.DeleteKey(key + 'G');
+        if (PlayerPrefs.HasKey(key + 'B')) PlayerPrefs.DeleteKey(key + 'B');
+        if (PlayerPrefs.HasKey(key + 'A')) PlayerPrefs.DeleteKey(key + 'A');
+    }
+
+    public static T SetEnum<T>(string key, T value) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
+    {
+        if (!typeof(T).IsEnum) throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
+        PlayerPrefs.SetInt(key + "_ENUM_" + typeof(T).ToString().ToUpper(), System.Convert.ToInt32(value));
         return value;
     }
     public static T GetEnum<T>(string key, T defaultValue) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
     {
-        if (!typeof(T).IsEnum)
-            throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
-        int intVal = PlayerPrefs.GetInt(key + "ENUM" + typeof(T).ToString(), System.Convert.ToInt32(defaultValue));
-        T retVal = (T)(object)intVal;
-        return SetEnum(key + "ENUM" + typeof(T).ToString(), retVal);
+        if (!typeof(T).IsEnum) throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
+        return SetEnum(key, (T)(object)PlayerPrefs.GetInt(key + "_ENUM_" + typeof(T).ToString().ToUpper(), System.Convert.ToInt32(defaultValue)));
     }
-    public static T SetEnum<T>(string key, T value) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
+    public static T GetEnum<T>(string key) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
     {
-        if (!typeof(T).IsEnum)
-            throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
-        PlayerPrefs.SetInt(key + "ENUM" + typeof(T).ToString(), System.Convert.ToInt32(value));
+        if (!typeof(T).IsEnum) throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
+        return SetEnum(key, (T)(object)PlayerPrefs.GetInt(key + "_ENUM_" + typeof(T).ToString().ToUpper(), 0));
+    }
+    public static T GetEnum<T>(string key, ref T value) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
+    {
+        if (!typeof(T).IsEnum) throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
+        return value = GetEnum(key, value);
+    }
+    public static void ResetEnum<T>(string key) where T : struct, System.IFormattable, System.IConvertible, System.IComparable
+    {
+        if (!typeof(T).IsEnum) throw new System.ArgumentException(typeof(T).ToString() + " is not an enum");
+        key += "_ENUM_" + typeof(T).ToString().ToUpper();
+        if (PlayerPrefs.HasKey(key)) PlayerPrefs.DeleteKey(key);
+    }
+
+    public static Vector3 SetVector3(string key, Vector3 value)
+    {
+        key += "_VECTOR3_";
+        PlayerPrefs.SetFloat(key + 'X', value.x);
+        PlayerPrefs.SetFloat(key + 'Y', value.y);
+        PlayerPrefs.SetFloat(key + 'Z', value.z);
         return value;
     }
+    public static Vector3 GetVector3(string key, Vector3 defaultValue)
+    {
+        Vector3 ret;
+        ret.x = PlayerPrefs.GetFloat(key + "_VECTOR3_X", defaultValue.x);
+        ret.y = PlayerPrefs.GetFloat(key + "_VECTOR3_Y", defaultValue.y);
+        ret.z = PlayerPrefs.GetFloat(key + "_VECTOR3_Z", defaultValue.z);
+        return SetVector3(key, ret);
+    }
+    public static Vector3 GetVector3(string key)
+    {
+        return GetVector3(key, Vector3.zero);
+    }
+    public static Vector3 GetVector3(string key, ref Vector3 value)
+    {
+        return value = GetVector3(key, value);
+    }
+    public static void ResetVector3(string key)
+    {
+        key += "_VECTOR3_";
+        if (PlayerPrefs.HasKey(key + 'X')) PlayerPrefs.DeleteKey(key + 'X');
+        if (PlayerPrefs.HasKey(key + 'Y')) PlayerPrefs.DeleteKey(key + 'Y');
+        if (PlayerPrefs.HasKey(key + 'Z')) PlayerPrefs.DeleteKey(key + 'Z');
+    }
+
+    public static Vector2 SetVector2(string key, Vector2 value)
+    {
+        key += "_VECTOR2_";
+        PlayerPrefs.SetFloat(key + 'X', value.x);
+        PlayerPrefs.SetFloat(key + 'Y', value.y);
+        return value;
+    }
+    public static Vector2 GetVector2(string key, Vector2 defaultValue)
+    {
+        Vector2 ret;
+        ret.x = PlayerPrefs.GetFloat(key + "_VECTOR2_X", defaultValue.x);
+        ret.y = PlayerPrefs.GetFloat(key + "_VECTOR2_Y", defaultValue.y);
+        return SetVector2(key, ret);
+    }
+    public static Vector2 GetVector2(string key)
+    {
+        return GetVector2(key, Vector2.zero);
+    }
+    public static Vector2 GetVector2(string key, ref Vector2 value)
+    {
+        return value = GetVector2(key, value);
+    }
+    public static void ResetVector2(string key)
+    {
+        key += "_VECTOR2_";
+        if (PlayerPrefs.HasKey(key + 'X')) PlayerPrefs.DeleteKey(key + 'X');
+        if (PlayerPrefs.HasKey(key + 'Y')) PlayerPrefs.DeleteKey(key + 'Y');
+    }
+
+    public static Quaternion SetQuaternion(string key, Quaternion value)
+    {
+        key += "_QUATERNION_";
+        PlayerPrefs.SetFloat(key + 'X', value.x);
+        PlayerPrefs.SetFloat(key + 'Y', value.y);
+        PlayerPrefs.SetFloat(key + 'Z', value.z);
+        PlayerPrefs.SetFloat(key + 'W', value.w);
+        return value;
+    }
+    public static Quaternion GetQuaternion(string key, Quaternion defaultValue)
+    {
+        Quaternion ret;
+        ret.x = PlayerPrefs.GetFloat(key + "_QUATERNION_X", defaultValue.x);
+        ret.y = PlayerPrefs.GetFloat(key + "_QUATERNION_Y", defaultValue.y);
+        ret.z = PlayerPrefs.GetFloat(key + "_QUATERNION_Z", defaultValue.z);
+        ret.w = PlayerPrefs.GetFloat(key + "_QUATERNION_W", defaultValue.w);
+        return SetQuaternion(key, ret);
+    }
+    public static Quaternion GetQuaternion(string key)
+    {
+        return GetQuaternion(key, Quaternion.identity);
+    }
+    public static Quaternion GetQuaternion(string key, ref Quaternion value)
+    {
+        return value = GetQuaternion(key, value);
+    }
+    public static void ResetQuaternion(string key)
+    {
+        key += "_QUATERNION_";
+        if (PlayerPrefs.HasKey(key + 'X')) PlayerPrefs.DeleteKey(key + 'X');
+        if (PlayerPrefs.HasKey(key + 'Y')) PlayerPrefs.DeleteKey(key + 'Y');
+        if (PlayerPrefs.HasKey(key + 'Z')) PlayerPrefs.DeleteKey(key + 'Z');
+        if (PlayerPrefs.HasKey(key + 'W')) PlayerPrefs.DeleteKey(key + 'W');
+    }
+
     public static void Save()
     {
         PlayerPrefs.Save();
     }
-    public static void Reset()
+    public static void ResetAll()
     {
         PlayerPrefs.DeleteAll();
     }
