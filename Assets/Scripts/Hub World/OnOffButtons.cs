b5a5e75feae6e64719245e971af223a20f630b03
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-10-26 14:57:10 -0400

    changed buttons in gameplay settings menu from toggle switches to on/off buttons

---
+++ Assets/Scripts/Hub World/OnOffButtons.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+public class OnOffButtons : MonoBehaviour
+{
+    [SerializeField] private EventSelectedObject onButton = null, offButton = null;
+    [SerializeField] private MeshRenderer onButtonRenderer = null, offButtonRenderer = null;
+    [SerializeField] private Material activeMaterial = null, inactiveMaterial = null;
+    public delegate void ButtonPressedEvent(bool isOn);
+    public ButtonPressedEvent OnButtonPressed;
+    private void Awake()
+    {
+        onButtonRenderer = onButtonRenderer ?? onButton.GetComponent<MeshRenderer>();
+        offButtonRenderer = offButtonRenderer ?? offButton.GetComponent<MeshRenderer>();
+    }
+    private void OnEnable()
+    {
+        onButton.OnSelectSuccess += PressedOnButton;
+        offButton.OnSelectSuccess += PressedOffButton;
+    }
+    private void OnDisable()
+    {
+        onButton.OnSelectSuccess -= PressedOnButton;
+        offButton.OnSelectSuccess -= PressedOffButton;
+    }
+    public bool SetValue(bool value)
+    {
+        if (null != onButtonRenderer)
+            onButtonRenderer.material = value ? activeMaterial : inactiveMaterial;
+        if (null != offButtonRenderer)
+            offButtonRenderer.material = value ? inactiveMaterial : activeMaterial;
+        return value;
+    }
+    private void PressedOnButton()
+    {
+        if (null != OnButtonPressed)
+            OnButtonPressed(SetValue(true));
+    }
+    private void PressedOffButton()
+    {
+        if (null != OnButtonPressed)
+            OnButtonPressed(SetValue(false));
+    }
+}
