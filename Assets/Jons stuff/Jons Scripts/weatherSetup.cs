02eaea108bef9df01024d96529af0ed4dd4b9655
Jonathan Ribarro <Jcribarro@gmail.com>
2017-10-30 16:44:28 -0400

    Made a better cloud for rainys and made the effects turn off and added a tag

--- Assets/Jons stuff/Jons Scripts/weatherSetup.cs
+++ Assets/Jons stuff/Jons Scripts/weatherSetup.cs
@@ -9,12 +9,13 @@ public class weatherSetup : MonoBehaviour {
     private GameObject player;
     private GameObject rain;
     private GameObject snow;
+    private GameObject cloud;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         rain = GameObject.FindGameObjectWithTag("rain");
         snow = GameObject.FindGameObjectWithTag("snow");
-
+        cloud = GameObject.FindGameObjectWithTag("cloud");
 	}
 	
     void getCorrectWeather()
@@ -26,28 +27,40 @@ public class weatherSetup : MonoBehaviour {
               //sunny
               case 0: 
                         //take away any particle systems besides dust 
-                        rain.GetComponent<ParticleSystem>().Stop(); 
+                        rain.GetComponent<ParticleSystem>().Stop();
+                    snow.GetComponent<ParticleSystem>().Stop();
+                    cloud.GetComponent<ParticleSystem>().Stop();
+
                     break;
                   //rainy
               case 1: 
                         //add rain particle system
                         rain.GetComponent<ParticleSystem>().Play();
-   
+                    cloud.GetComponent<ParticleSystem>().Play();
                   break;
                   //snowy
               case 2: 
                         //add and make active snow particle system 
-                        snow.GetComponent<ParticleSystem>().Play(); 
-                  break;
+                        snow.GetComponent<ParticleSystem>().Play();
+                    cloud.GetComponent<ParticleSystem>().Play();
+
+                    break;
 
                 default:
                     {
                         rain.GetComponent<ParticleSystem>().Stop();
                         snow.GetComponent<ParticleSystem>().Stop();
+                        cloud.GetComponent<ParticleSystem>().Stop();
+
                     }
                     break;
           }
       }
+      else
+        {
+            rain.GetComponent<ParticleSystem>().Stop();
+            snow.GetComponent<ParticleSystem>().Stop();
+        }
   }
   
 	// Update is called once per frame
