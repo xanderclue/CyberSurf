260dcb9d2665e8b1086c4c5e62ef30f06e4e74cc
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-11-08 13:08:49 -0500

    some changes to the boards and board stands

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -1,17 +1,17 @@
 ﻿public static class EventManager
 {
-    public delegate void IntParamEvent(int sceneIndex);
-    public delegate void BoolParamEvent(bool isOn);
+    public delegate void IntParamEvent(int value);
+    public delegate void BoolParamEvent(bool value);
     public delegate void VoidParamEvent();
     public static event IntParamEvent OnTransition;
     public static event BoolParamEvent OnToggleMovement, OnSelectionLock, OnToggleHud, OnSetRingPath;
     public static event VoidParamEvent OnUpdateBoardMenuEffects, OnStartRingPulse, OnStopRingPulse;
-    public static void OnSetGameplayMovementLock(bool locked) { OnToggleMovement?.Invoke(locked); }
-    public static void OnTriggerTransition(int sceneIndex) { OnTransition?.Invoke(sceneIndex); }
-    public static void OnTriggerSelectionLock(bool locked) { OnSelectionLock?.Invoke(locked); }
-    public static void OnSetHudOnOff(bool isOn) { OnToggleHud?.Invoke(isOn); }
-    public static void OnCallSetRingPath(bool isOn) { OnSetRingPath?.Invoke(isOn); }
-    public static void OnCallBoardMenuEffects() { OnUpdateBoardMenuEffects?.Invoke(); }
-    public static void StartRingPulse() { OnStartRingPulse?.Invoke(); }
-    public static void StopRingPulse() { OnStopRingPulse?.Invoke(); }
+    public static void OnSetGameplayMovementLock(bool locked) => OnToggleMovement?.Invoke(locked);
+    public static void OnTriggerTransition(int sceneIndex) => OnTransition?.Invoke(sceneIndex);
+    public static void OnTriggerSelectionLock(bool locked) => OnSelectionLock?.Invoke(locked);
+    public static void OnSetHudOnOff(bool isOn) => OnToggleHud?.Invoke(isOn);
+    public static void OnCallSetRingPath(bool isOn) => OnSetRingPath?.Invoke(isOn);
+    public static void OnCallBoardMenuEffects() => OnUpdateBoardMenuEffects?.Invoke();
+    public static void StartRingPulse() => OnStartRingPulse?.Invoke();
+    public static void StopRingPulse() => OnStopRingPulse?.Invoke();
 }
