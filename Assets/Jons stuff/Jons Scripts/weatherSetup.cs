a4b0b41b63b84e8347114b9aa6f8802083b28091
Jonathan Ribarro <Jcribarro@gmail.com>
2017-11-14 11:15:13 -0500

    Fixed the canyon snow, have to change the rest of the levels too

--- Assets/Jons stuff/Jons Scripts/weatherSetup.cs
+++ Assets/Jons stuff/Jons Scripts/weatherSetup.cs
@@ -3,45 +3,52 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class weatherSetup : MonoBehaviour {
+public class weatherSetup : MonoBehaviour
+{
 
     private GameObject rainSystem;
     //private GameObject player;
     private GameObject rain;
-    private GameObject snow;
-    private GameObject cloud;
-	// Use this for initialization
-	void Start () {
-        //player = GameObject.FindGameObjectWithTag("Player");
+    private GameObject worldSnow;
+    private GameObject playerSnow;
+   private GameObject cloud;
+    // Use this for initialization
+    void Start()
+    {
+        worldSnow = GameObject.FindGameObjectWithTag("worldSnow");
+        playerSnow = GameObject.FindGameObjectWithTag("playerSnow");
+
         rain = GameObject.FindGameObjectWithTag("rain");
-        snow = GameObject.FindGameObjectWithTag("snow");
+
         cloud = GameObject.FindGameObjectWithTag("cloud");
-	}
-	
+    }
+
     void getCorrectWeather()
-  {
-      if(SceneManager.GetActiveScene().buildIndex > 1)
-      {
-          switch((int)WeatherOptions.ActualWeather)
-          {
-              //sunny
-              case 0: 
-                        //take away any particle systems besides dust 
-                        rain.GetComponent<ParticleSystem>().Stop();
-                    snow.GetComponent<ParticleSystem>().Stop();
+    {
+        if (SceneManager.GetActiveScene().buildIndex > 1)
+        {
+            switch ((int)WeatherOptions.ActualWeather)
+            {
+                //sunny
+                case 0:
+                    //take away any particle systems besides dust 
+                    rain.GetComponent<ParticleSystem>().Stop();
+                    worldSnow.GetComponent<ParticleSystem>().Stop();
+                    playerSnow.GetComponent<ParticleSystem>().Stop();
                     cloud.GetComponent<ParticleSystem>().Stop();
 
                     break;
-                  //rainy
-              case 1: 
-                        //add rain particle system
-                        rain.GetComponent<ParticleSystem>().Play();
+                //rainy
+                case 1:
+                    //add rain particle system
+                    rain.GetComponent<ParticleSystem>().Play();
                     cloud.GetComponent<ParticleSystem>().Play();
-                  break;
-                  //snowy
-              case 2: 
-                        //add and make active snow particle system 
-                        snow.GetComponent<ParticleSystem>().Play();
+                    break;
+                //snowy
+                case 2:
+                    //add and make active snow particle system 
+                    worldSnow.GetComponent<ParticleSystem>().Play();
+                    playerSnow.GetComponent<ParticleSystem>().Play();
                     cloud.GetComponent<ParticleSystem>().Play();
 
                     break;
@@ -49,22 +56,28 @@ public class weatherSetup : MonoBehaviour {
                 default:
                     {
                         rain.GetComponent<ParticleSystem>().Stop();
-                        snow.GetComponent<ParticleSystem>().Stop();
+                        worldSnow.GetComponent<ParticleSystem>().Stop();
+                        playerSnow.GetComponent<ParticleSystem>().Stop();
                         cloud.GetComponent<ParticleSystem>().Stop();
 
                     }
                     break;
-          }
-      }
-      else
+            }
+        }
+        else
         {
             rain.GetComponent<ParticleSystem>().Stop();
-            snow.GetComponent<ParticleSystem>().Stop();
+            if(worldSnow)
+            worldSnow.GetComponent<ParticleSystem>().Stop();
+            playerSnow.GetComponent<ParticleSystem>().Stop();
         }
-  }
-  
-	// Update is called once per frame
-	void FixedUpdate () {
-        getCorrectWeather();
-	}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+            getCorrectWeather();
+        
+    }
 }
