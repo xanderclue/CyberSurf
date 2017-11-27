bf5ec15eaa7d30ef5eccc680351bda8d255e093a
Jonathan Ribarro <Jcribarro@gmail.com>
2017-11-27 11:48:41 -0500

    changed a few things and added Kris's design for animated cybersurf logo and other FS logo

--- Assets/Scripts/LevelOptions/LevelSelectOptions.cs
+++ Assets/Scripts/LevelOptions/LevelSelectOptions.cs
@@ -12,6 +12,8 @@ public class LevelSelectOptions : LevelMenuObjectGroup
     [SerializeField] private Sprite multiEnrironmentImage = null;
     [SerializeField] private Sprite backyardRacetrackImage = null;
     [SerializeField] private Sprite computerChipImage = null;
+    [SerializeField]   private Sprite cityImage = null;
+
     [SerializeField] private LevelManager.Level defaultLevel = LevelManager.Level.Canyon;
     private LevelManager.Level tempLevel;
     [SerializeField] private WorldPortalProperties portal = null;
@@ -90,6 +92,9 @@ public class LevelSelectOptions : LevelMenuObjectGroup
                 break;
             case LevelManager.Level.ComputerChip:
                 levelImage.sprite = computerChipImage;
+                break;
+            case LevelManager.Level.City:
+                levelImage.sprite = cityImage;
                 break;
             default:
                 Debug.LogWarning("Switch statement on Level enum tempLevel in LevelSelectOptions.cs is missing case for Level." + tempLevel.ToString() + this.Info(), this);
