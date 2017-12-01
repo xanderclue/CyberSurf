5953fe2be1fd090211a91a35d191469b14fcd3bf
Matthew Myers <mrmyers1@fullsail.edu>
2017-12-01 17:09:46 -0500

    text above exit ring changed
    May not be working on all levels

---
+++ Assets/Scripts/Ring Scripts/Race_Lap_Ring_Text_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Race_Lap_Ring_Text_Script : MonoBehaviour {
+
+
+    private TextMeshPro element = null;
+    private Lap_Text_script laptext = null;
+    private void Start()
+    {
+        element = GetComponent<TextMeshPro>();
+        laptext = GameObject.FindObjectOfType<Lap_Text_script>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (GameModes.Race == GameManager.instance.gameMode.currentMode && laptext.curr_lap != laptext.max_lap)
+        {
+            element.SetText("Checkpoint");
+        }
+        else
+        {
+            element.SetText("Exit");
+        }
+	}
+}
