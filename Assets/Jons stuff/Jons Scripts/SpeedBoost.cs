a02f3f3cfe32481b3e47e0cf495b605c3c21ee78
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-12-13 16:43:37 -0500

    meh.. nuu

--- Assets/Jons stuff/Jons Scripts/SpeedBoost.cs
+++ Assets/Jons stuff/Jons Scripts/SpeedBoost.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedBoostScript : MonoBehaviour {
+public class SpeedBoost : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
