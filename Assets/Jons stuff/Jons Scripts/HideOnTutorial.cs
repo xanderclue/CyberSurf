0b0704063d0b524b0667924e278c8670d1a448c2
Xander Kyle Puckett <xanderkylepuckett@gmail.com>
2017-12-14 01:55:00 -0500

    HideOnTutorial

---
+++ Assets/Jons stuff/Jons Scripts/HideOnTutorial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class HideOnTutorial : MonoBehaviour
+{
+    [SerializeField, LabelOverride("Objects")] private GameObject[] objs = null;
+    private void Start()
+    {
+        if (keepPlayerStill.tutorialOn)
+            foreach (GameObject obj in objs)
+                obj.SetActive(false);
+        else Destroy(gameObject);
+    }
+    private void Update()
+    {
+        if (!keepPlayerStill.tutorialOn)
+        {
+            foreach (GameObject obj in objs)
+                obj.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
