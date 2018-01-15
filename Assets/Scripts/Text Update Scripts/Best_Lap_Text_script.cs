40ca39fd4487f8d28eb68575db8818a91e92e239
Matthew Myers <mrmyers1@fullsail.edu>
2018-01-15 15:10:12 -0500

    Half rings disabled and more timer stuff

--- Assets/Scripts/Text Update Scripts/Best_Lap_Text_script.cs
+++ Assets/Scripts/Text Update Scripts/Best_Lap_Text_script.cs
@@ -1,2 +1,33 @@
 ﻿using UnityEngine;
-public class Best_Lap_Text_script : MonoBehaviour { }
+using TMPro;
+public class Best_Lap_Text_script : MonoBehaviour {
+
+    public float[] times;
+    private int curr_lap;
+    private Lap_Text_script LT;
+    private TextMeshProUGUI element = null;
+
+    private void Start()
+    {
+        element = GetComponent<TextMeshProUGUI>();
+        times = new float[5];
+
+    }
+    public void Update_text()
+    {
+        string text_to_write = "Best Lap" + '\n';
+        for (int i = 0; i < times.Length; i++)
+        {
+            text_to_write += times[i].ToString("n2") + '\n';
+        }
+        element.SetText(text_to_write);
+    }
+    public void Cleanup()
+    {
+        for (int i = 0; i < times.Length; i++)
+        {
+            times[i] = 0.00f;
+        }
+        Update_text();
+    }
+}
