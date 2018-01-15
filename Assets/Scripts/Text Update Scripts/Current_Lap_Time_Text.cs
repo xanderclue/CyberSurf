a7bb2180157cf90b73b8448bbe316c9e6cdf0b87
Matthew Myers <mrmyers1@fullsail.edu>
2018-01-15 11:54:20 -0500

    Timers working

--- Assets/Scripts/Text Update Scripts/Current_Lap_Time_Text.cs
+++ Assets/Scripts/Text Update Scripts/Current_Lap_Time_Text.cs
@@ -1,2 +1,27 @@
 ﻿using UnityEngine;
-public class Current_Lap_Time_Text : MonoBehaviour { }
+using TMPro;
+public class Current_Lap_Time_Text : MonoBehaviour {
+
+    float Time_passed, Current;
+    int curr_lap;
+    private Lap_Text_script LT;
+    private TextMeshProUGUI element = null;
+
+    private void Start()
+    {
+        element = GetComponent<TextMeshProUGUI>();
+        LT = GameObject.FindObjectOfType<Lap_Text_script>();
+        Time_passed = 0;
+        curr_lap = 1;
+    }
+    private void Update()
+    {
+        Current = RoundTimer.timeInLevel;
+        element.SetText((Current - Time_passed).ToString("n2"));
+        if (curr_lap != LT.CurrLap)
+        {
+            curr_lap++;
+            Time_passed = Current;
+        }
+    }
+}
