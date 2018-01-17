27b9c39900b6f52eb7f450bf351bdc887c808660
Jonathan Ribarro <Jcribarro@gmail.com>
2018-01-17 11:43:03 -0500

    Various fixes as well as fixing a big problem in the hub world

---
+++ Assets/turnOffOuterCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffOuterCollider : MonoBehaviour {
+    GameObject outerCollider;
+	// Use this for initialization
+	void Start () {
+        outerCollider = GameObject.FindGameObjectWithTag("invisibleWalls");
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Board")
+        {
+            outerCollider.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Board")
+        {
+            outerCollider.gameObject.SetActive(true);
+        }
+    }
+    // private void OnTriggerEnter(Collision collision)
+    // {
+    //     
+    // }
+}
