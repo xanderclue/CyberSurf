09eeaeaadae323e53ef939015a8195dafbbb8402
Jonathan Ribarro <Jcribarro@gmail.com>
2018-02-09 11:21:42 -0500

    Fixed up a couple of mesh colliders

---
+++ Assets/Scripts/Managers/Phantom Racer Scripts/positionRecorder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class positionRecorder : MonoBehaviour
+{
+    public Queue<Vector3> positions = new Queue<Vector3>();
+    public Queue<Quaternion> rotations = new Queue<Quaternion>();
+    private void OnEnable() => SceneManager.sceneLoaded += Clear;
+    private void OnDisable() => SceneManager.sceneLoaded -= Clear;
+    private void Clear(Scene s, LoadSceneMode m)
+    {
+        positions.Clear();
+        rotations.Clear();
+    }
+    private void FixedUpdate()
+    {
+        positions.Enqueue(transform.position);
+        rotations.Enqueue(transform.rotation);
+    }
+}
